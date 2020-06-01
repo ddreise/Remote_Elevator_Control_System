1fd624d8b27da581169177ae5fdcf02c4f7fd7d8
ddreise <ddreise@gmail.com>
2020-06-01 07:45:10 -0400

    Rudimentary UI for call stations

--- php/call_stations.php
+++ php/call_stations.php
@@ -1,5 +1,5 @@
 <?php
-$submitted = !empty($_POST);
+$submitted = !empty($_GET);
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +10,24 @@ $submitted = !empty($_POST);
 
     <body>
         <p>Form submitted? <?php echo (int) $submitted; ?></p>
-        <p>Up button pressed:       <?php echo $_POST['up_arrow']; ?></p>
-        <p>Down button pressed:     <?php echo $_POST['down_arrow']; ?></p>
+        <h3>Floor 1</h3>
+        <ul>
+            <li>Up button pressed:  <?php echo $_GET['floor1_up_x']; ?></li>
+            <li>Down button pressed:  <?php echo $_GET['floor1_down_x']; ?></li>
+        </ul>
+
+        <h3>Floor 2</h3>
+        <ul>
+            <li>Up button pressed:  <?php echo $_GET['floor2_up_x']; ?></li>
+            <li>Down button pressed:  <?php echo $_GET['floor2_down_x']; ?></li>
+        </ul>
+
+        <h3>Floor 3</h3>
+        <ul>
+            <li>Up button pressed:  <?php echo $_GET['floor3_up_x']; ?></li>
+            <li>Down button pressed:  <?php echo $_GET['floor3_down_x']; ?></li>
+        </ul>
+
 
         <p>&copy; Daniel Dreise</p>
 
