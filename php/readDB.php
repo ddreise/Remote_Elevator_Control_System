bfb6fed7f2c1274880354e89377653c955d543e3
Justin Turcotte <justin@kturcotte.me>
2020-06-26 17:20:48 +0000

    Added readDB.php

---
+++ php/readDB.php
@@ -0,0 +1,16 @@
+<?php
+$db = new PDO(
+        'mysql:host=127.0.0.1;dbname=elevator',
+        'admin',
+        'raspberry'
+    );
+    // Return arrays with keys that are the name of the fields
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $rows = $db->query('SELECT * FROM elevatorNetwork ORDER BY nodeID');
+    foreach($rows as $row)
+    {
+        var_dump($row);
+        echo"<br/>";
+    }
+?>
