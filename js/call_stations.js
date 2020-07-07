77a5100a8e323f30adf004dc4d3c376314afbb5b
Daniel Dreise <ddreise@gmail.com>
2020-07-06 21:40:23 -0400

    Still need to debug AJAX

--- js/call_stations.js
+++ js/call_stations.js
@@ -1,9 +1,10 @@
-var $floor1_up = document.getElementById("floor1_up");
+/* var $floor1_up = document.getElementById("floor1_up");
 var $floor2_up = document.getElementById("floor2_up");
 var $floor2_down = document.getElementById("floor2_down");
-var $floor3_down = document.getElementById("floor3_down");
+var $floor3_down = document.getElementById("floor3_down"); */
 
-var elfloor = document.getElementById('floor');
+var elfloor = document.getElementById("current_floor");
+
 
 // Function to get the current floor number
 function showFloor() {
@@ -13,19 +14,21 @@ function showFloor() {
     xmlhttpShow.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
             var resp = this.responseText;
-            document.getElementById('floor').innerHTML = resp;
+            document.getElementById("floor").innerHTML = resp;
         }
     };
 
-    xmlhttpShow.open ("GET", "../php/call_stations.php?q=", true);          // access PHP file for information
-    xmlhttpShow.send();
+    xmlhttpShow.open ("GET", "test.txt", true);          // access PHP file for information
+    xmlhttpShow.send();                                                     // Send request
 }
 
 function showFloorInterval(millisec) {
     setInterval(showFloor, millisec);
 }
 
-current_floor.addEventListener('click', function() {showFloor()}, false);
-window.addEventListener('load', function() {showFloorInterval(3000)}, false);
+
+window.addEventListener("load", function() {showFloorInterval(3000)}, false);
+elfloor.addEventListener("click", function() {showFloor()}, false);
+
 
 
