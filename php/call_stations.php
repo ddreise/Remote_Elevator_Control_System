22759d9cf961087fc7aac6658262cec63e48e193
TAnnette3903 <tannette3903@conestogac.on.ca>
2020-07-07 10:31:00 -0400

    Removed and updated duplicated call_stations.php

--- php/call_stations.php
+++ php/call_stations.php
@@ -1,126 +1,162 @@
-<?php
-    $submitted = !empty($_POST);
-    $floor1 = 1;
-    $floor2 = 2;
-    $floor3 = 3;
-?>
-
-<!-- Function for getting current floor number. Taken from Michael Galle's code -->
-<?php 
-        function get_currentFloor(): int {
-                try { $db = new PDO('mysql:host=127.0.0.1;dbname=elevator','ese','ese');}
-                catch (PDOException $e){echo $e->getMessage();}
-
-                        // Query the database to display current floor
-                        $rows = $db->query('SELECT currentFloor FROM elevatorNetwork');
-                        foreach ($rows as $row) {
-                                $current_floor = $row[0];
-                        }
-                        return $current_floor;
-        }
-?>
-<?php
-        function update_elevatorNetwork(int $node_ID, int $new_floor =1): int {
-                $db1 = new PDO('mysql:host=127.0.0.1;dbname=elevator','ese','ese');
-                $query = 'UPDATE elevatorNetwork 
-                                SET currentFloor = :floor
-                                WHERE nodeID = :id';
-                $statement = $db1->prepare($query);
-                $statement->bindvalue('floor', $new_floor);
-                $statement->bindvalue('id', $node_ID);
-                $statement->execute();  
-
-                return $new_floor;
-        }
-?>
-
-
-<!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Call Stations</title>
+        <meta name="description" content="This is the Request Access page" />
+        <meta name="robots" content="noindex nofollow" /> <!-- do not want page/links to be indexed-->
+        <meta http-equiv="author" content="Daniel Dreise" />
+        <meta http-equiv="pragma" content="no-cache" /> <!-- want browser to not store cache -->
+        
+        <!-- For Bootstrap -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">      
+    
+        <!-- For CSS -->
+        <link href="css/dans_project_style.css" type="text/css" rel="stylesheet" />
     </head>
 
     <body>
-        <h1>Call Stations</h1> 
+        <div id="page" class="container">
+            <header>
+                <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+                    <!-- Enable navbar buttons-->
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-        <h2>Current Floor</h2>
-        <fiedset>
-            <!-- need to compensate for floor1_up convert to number -->
-            <?php 
-                    if(isset($_POST['floor1_up'||'floor1_down'])){
-                        $curFlr = update_elevatorNetwork(1, $floor1); 
-                        header('Refresh:0; url=php/call_stations.php');
-                    }
-                    $curFlr = get_currentFloor();
-                    echo "<h2>Current floor # $curFlr </h2>";
-            ?>
-        </fielset>
+                    <!--Logo-->
+                    <a class="navbar-brand" href="#"><img src="images/dan-avatar.png" class="img-responsive" alt="Temporary logo", title="Temporary logo" height=40px/>  ESE Project VI</a>
 
-        <h2>Floor Request</h2>  
-        <form action="php/call_stations.php" method="POST">
-                Request floor # <input type="number" style="width:50px; height:40px" name="newfloor" max=3 min=1 required />
-                <input type="submit" value="Go"/>
-        </form>
+                    <!--Menu buttons-->
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul id="header_menu" class="navbar-nav mr-auto" > 
+                                    <li class="h_menu nav-item"><a class="menu nav-link" href="index.html">Home</a></li>
+                                    <li class="h_menu nav-item"><a class="menu nav-link" href="about.html">About</a></li>
+                                    <li class="h_menu nav-item"><a class="menu nav-link" href="project_plan.html">Project Plan</a></li>
+                                    <li class="h_menu nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="logbooksDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Logbooks</a>
+                                        <div class="dropdown-menu" aria-labelledby="logbooksDropdown">
+                                            <a class="dropdown-item" href="daniel-logbook.html">Dan's Logbook</a>
+                                            <a class="dropdown-item" href="brandon-logbook.html">Brandon's Logbook</a>
+                                            <a class="dropdown-item" href="justin-logbook.html">Justin's Logbook</a>
+                                            <a class="dropdown-item" href="troy-logbook.html">Troy's Logbook</a>
+                                        </div>
+                                    </li>
+                                    <li class="h_menu nav-item dropdown">
+                                        <a class="menu nav-link dropdown-toggle" href="#" id="minutesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Meeting Minutes</a>
+                                        <div class="dropdown-menu" aria-labelledby="minutesDropdown">
+                                            <a class="dropdown-item" href="meeting-minutes.html">Meeting Minutes</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="meeting-minutes.html#week1">Week 1</a>
+                                            <a class="dropdown-item" href="meeting-minutes.html#week2">Week 2</a>
+                                            <a class="dropdown-item" href="meeting-minutes.html#week3">Week 3</a>
+                                            <a class="dropdown-item" href="meeting-minutes.html#week4">Week 4</a>
+                                        </div>
+                                    </li>
+                                    <li class="h_menu nav-item"><a class="menu nav-link" href="videos.html">Videos</a></li>
+                                    <li class="h_menu nav-item"><a class="menu nav-link" href="#">Documents</a></li>
+                                    <li class="h_menu nav-item"><a class="menu nav-link" href="call_stations.html">Elevator GUI</a></li>
+                                </ul>
+                    </div>
+                            
+                </nav>
 
-        <form action="php/call_stations.php" method="POST">
-            <h2>Call Elevator</h2>
-            <fieldset>
-                <legend>Floor 1</legend>
-                    <!--Up arrow button image-->
-                    <input name="floor1_up" type="image" src="images/up_arrow.png" value="UP" alt="up_arrow" width="40"/>
+           
 
-                    <!--Down arrow button image-->
-                    <input name="floor1_down" type="image" src="images/down_arrow.png" value="DOWN" alt="down_arrow" width="40"/>
-                </p> 
-            </fieldset>
+                <div class="jumbotron text-center">
+                        <h1>Project VI - Elevator GUI</h1>
+                </div>
 
-            <fieldset>
-                <legend>Floor 2</legend>
-                    <!--Up arrow button image-->
-                    <input name="floor2_up" type="image" src="images/up_arrow.png" value="UP" alt="up_arrow" width="40"/>
-
-                    <!--Down arrow button image-->
-                    <input name="floor2_down" type="image" src="images/down_arrow.png" value="DOWN" alt="down_arrow" width="40"/>
-                </p> 
-            </fieldset>
-
-            <fieldset>
-                <legend>Floor 3</legend>
-                    <!--Up arrow button image-->
-                    <input name="floor3_up" type="image" src="images/up_arrow.png" value="UP" alt="up_arrow" width="40"/>
-
-                    <!--Down arrow button image-->
-                    <input name="floor3_down" type="image" src="images/down_arrow.png" value="DOWN" alt="down_arrow" width="40"/>
-                </p> 
-            </fieldset>
-        </form>
-
+            </header>
 
 
-        <!--
-        <p>Form submitted? <?php echo (int) $submitted; ?></p>
-        <h3>Floor 1</h3>
-        <ul>
-            <li>Up button pressed:  <?php echo $_GET['floor1_up_x']; ?></li>
-            <li>Down button pressed:  <?php echo $_GET['floor1_down_x']; ?></li>
-        </ul>
+            <div id="content">
+                <article>
+                    <section>
+                        <div class="row">
+                            <div class="col-xs">
+                                <h2>Current Floor</h2>
+                                <fieldset>
+                                    <?php 
+                                        include "php/elevator_control.php"; 
+                                        $curFlr = get_currentFloor();
+                                        echo $curFlr;
+                                    ?>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </section>
+                </article>
+                
+                <!--Up/Down Request -->
+                <!--
+                <iframe name="up_down_request" style="display:none;"></iframe> Prevents new window opening (along with target=
+                -->
+                <article>
+                    <form action="elevator_control.php" method="get" id="access" target="up_down_request">
+                    <h2>Call Station Controls</h2>
+                        <div class="row">
+                            <div id="queue" class="col-md-3">
+                                <p>This space reserved for call queue</p>
+                            </div>
 
-        <h3>Floor 2</h3>
-        <ul>
-            <li>Up button pressed:  <?php echo $_GET['floor2_up_x']; ?></li>
-            <li>Down button pressed:  <?php echo $_GET['floor2_down_x']; ?></li>
-        </ul>
+                            <div class="col-md-3">
+                                <p>This space reserved for elevator position image</p>
+                            </div>
 
-        <h3>Floor 3</h3>
-        <ul>
-            <li>Up button pressed:  <?php echo $_GET['floor3_up_x']; ?></li>
-            <li>Down button pressed:  <?php echo $_GET['floor3_down_x']; ?></li>
-        </ul>
+                            <div class="col-md-3">
+                                <p>This space reserved for call station door images</p>
+                            </div>
 
-            -->
+                            <div class="col-md-3">
+                            
+                                <section>
+                                    <fieldset>
+                                        <legend>Floor 3</legend>        
+                                            <!--Down arrow button image-->
+                                            <input class="call_station_uparrow" name="floor3_down" type="image" src="images/call_station_downarrow.png" value="DOWN" alt="down_arrow" width="80"/>
+                                    </fieldset>
+                                </section>
+        
+                                <section>
+                                    <fieldset>
+                                        <legend>Floor 2</legend>
+                                            <!--Up arrow button image-->
+                                            <input class="call_station_uparrow" name="floor2_up" type="image" src="images/call_station_uparrow.png" value="UP" alt="up_arrow" width="80"/><br>
+        
+                                            <!--Down arrow button image-->
+                                            <input class="call_station_downarrow" name="floor2_down" type="image" src="images/call_station_downarrow.png" value="DOWN" alt="down_arrow" width="80"/>
+                                    </fieldset>
+                                </section>
+        
 
-        <p>&copy; Daniel Dreise</p>
+                                <section> 
+                                    <fieldset>
+                                        <legend>Floor 1</legend>
+                                            <!--Up arrow button image-->
+                                            <input class="call_station_uparrow" name="floor1_up" type="image" src="images/call_station_uparrow.png" value="UP" alt="up_arrow" width="80"/>
+                                    </fieldset>
+                                </section>
+
+                                <section>
+
+                                </section>
+                            </div>
+                 
+                        </div>
+        
+                    </form>
+                </article> 
+            </div>
+
+            <footer>
+                <p>&copy; Daniel Dreise</p>
+            </footer>
+        </div>
+
+        <!-- jQuery & JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
 
     </body>
 </html>
---
+++ php/elevator_control.php
@@ -0,0 +1,55 @@
+<?php
+    $submitted = !empty($_POST);
+    $floor1 = 1;
+    $floor2 = 2;
+    $floor3 = 3;
+?>
+
+<?php 
+    // Get the current floor number. Adapted from Michael Galle's code
+    function get_currentFloor(): int {
+            try { $db = new PDO('mysql:host=127.0.0.1;dbname=project-VI','admin','raspberry');}
+            catch (PDOException $e){echo $e->getMessage();}
+
+                    // Query the database to display current floor
+                    $rows = $db->query('SELECT currentFloor FROM elevatorNetwork');
+                    foreach ($rows as $row) {
+                            $current_floor = $row[0];
+                    }
+                    return $current_floor;
+    }
+
+    // Update the elevator network with the current target floor
+    function update_elevatorNetwork(int $node_ID, int $new_floor): int {
+            $db1 = new PDO('mysql:host=127.0.0.1;dbname=project-VI','admin','raspberry');
+            $query = 'UPDATE elevatorNetwork 
+                            SET currentFloor = :floor
+                            WHERE nodeID = :id';
+            $statement = $db1->prepare($query);
+            $statement->bindvalue('floor', $new_floor);
+            $statement->bindvalue('id', $node_ID);
+            $statement->execute();  
+
+            return $new_floor;
+    }
+
+    // Check for floor requests upon loading the page
+    function get_floorRequest(): 
+    if(isset($_POST['floor1_up'])){
+        update_elevatorNetwork(1, 2); 
+        header('Refresh:0; url=php/call_stations.php');
+    }
+    else if(isset($_POST['floor2_up'])){
+        update_elevatorNetwork(1, 3); 
+        header('Refresh:0; url=php/call_stations.php');
+    }
+    else if(isset($_POST['floor2_down'])){
+        update_elevatorNetwork(1, 1); 
+        header('Refresh:0; url=php/call_stations.php');
+    }
+    else if(isset($_POST['floor3_down'])){
+        update_elevatorNetwork(1, 2); 
+        header('Refresh:0; url=php/call_stations.php');
+    }
+    //$curFlr = get_currentFloor();
+?>
