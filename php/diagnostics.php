f5f35d6f69edb4766dd9cc3cef4b9f3fbb12253f
Valend <Valend@users.noreply.github.com>
2020-08-10 00:26:50 -0400

    modifications to diagnostic to make it independent of queue

--- php/diagnostics.php
+++ php/diagnostics.php
@@ -9,39 +9,58 @@ $db = new PDO(
 // Return arrays with keys that are the name of the fields.
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$rows = $db->query('SELECT * FROM elevatorQueue ORDER BY queueNumber');
-$rowFloor = 0;
-$rowDirecton = 0;
-$rowDoors = 0;
-$rowNodeID = 0;
-$f1Visits = 0;
-$f2Visits = 0;
-$f3Visits = 0;
+// $rowFloor = 0;
+// $directon;
+// $rowDoors;
+// $rowNodeID = 0;
+// $f1Visits = 0;
+// $f2Visits = 0;
+// $f3Visits = 0;
 
-foreach ($rows as $row)
+$queueID = 0;
+$direction = "";
+$doors = "";
+$floor1Visits = 0;
+$floor2Visits = 0;
+$floor3Visits = 0;
+
+$diagnostics = $db->query('SELECT * FROM elevatorDiagnostics ORDER BY nodeID');
+foreach ($diagnostics as $diagnostic)
 {
-    if($row['queueNumber'] > $rowNodeID)
-    {
-        $rowFloor = $row['destinationFloor'];
-        //$rowDirection = $row['direction'];
-        //$rowDoors = $row['doors'];
-        $rowNodeID = $row['queueNumber'];
-    }   
-
-    switch($row['destinationFloor'])
-    {
-        case 1:
-            $f1Visits++;
-        break;
-        case 2:
-            $f2Visits++;
-        break;
-        case 3:
-            $f3Visits++;
-    }
+    $queueID = $diagnostic['queueID'];
+    $direction = $diagnostic['direction'];
+    $doors = $diagnostic['doors'];
+    $floor1Visits = $diagnostic['floor1Visits'];
+    $floor2Visits = $diagnostic['floor2Visits'];
+    $floor3Visits = $diagnostic['floor3Visits'];
 }
 
-$output = [$rowFloor, $rowDirection, $rowDoors, $rowNodeID, $f1Visits, $f2Visits, $f3Visits];
+// $rows = $db->query('SELECT * FROM elevatorQueue ORDER BY queueNumber');
+
+// foreach ($rows as $row)
+// {
+//     if($row['queueNumber'] > $rowNodeID)
+//     {
+//         $rowFloor = $row['destinationFloor'];
+//         //$rowDirection = $row['direction'];
+//         //$rowDoors = $row['doors'];
+//         $rowNodeID = $row['queueNumber'];
+//     }   
+
+//     switch($row['destinationFloor'])
+//     {
+//         case 1:
+//             $f1Visits++;
+//         break;
+//         case 2:
+//             $f2Visits++;
+//         break;
+//         case 3:
+//             $f3Visits++;
+//     }
+// }
+
+$output = [$queueID, $direction, $doors, $floor1Visits, $floor2Visits, $floor3Visits];
 
 echo json_encode($output);
 
